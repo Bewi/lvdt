efa7502a6916d8d8629c116c06e4530cfe5aade0
bwilfart <benjamin.wilfart@staff.voo.be>
2017-03-09 23:00:47 +0100

    Init mobx

--- src/app/header/HeaderComponent.jsx
+++ src/app/header/HeaderComponent.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import {observer} from 'mobx-react';
 
+@observer
 class HeaderComponent extends Component {
     render() {
         return (
-            <header>Bois ton thé !</header>
+            <header>Bois ton thé ! {this.props.appState.timer}</header>
         )
     }
 }
