98901d958146959f8b8dc5af6ac9e29018d1958c
bwilfart <benjamin.wilfart@staff.voo.be>
2017-03-11 10:18:00 +0100

    Header layout

--- src/app/header/HeaderComponent.jsx
+++ src/app/header/HeaderComponent.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import {observer} from 'mobx-react';
 
+import Hero from './HeroComponent.jsx';
+import Search from './SearchComponent.jsx';
+
 @observer
 class HeaderComponent extends Component {
     render() {
         return (
-            <header>Bois ton thé ! {this.props.appState.timer}</header>
+            <header>
+                <Hero />
+                <Search />
+            </header>
         )
     }
 }
