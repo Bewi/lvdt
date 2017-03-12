6b0e4e05e45d0708735fd6094500e4a973ee2c29
bwilfart <benjamin.wilfart@staff.voo.be>
2017-03-12 10:30:12 +0100

    Gulp php :satisfied:

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
 
+var connect = require('gulp-connect-php');
+
 gulp.task('sassify', function() {
     return gulp.src('./src/sass/app.scss')
         .pipe(sourcemaps.init())
@@ -32,4 +34,17 @@ gulp.task('babel:watch', function() {
 });
 
 gulp.task('watch', ['sassify', 'babelify', 'sass:watch', 'babel:watch']);
+
+
+
+// EXPERIMENTAL
+gulp.task('phpify', function() {
+    connect.server({
+      hostname: 'localhost',
+      bin: 'C:/php/php.exe',
+      ini: 'C:/php/php.ini',
+      port: 8000,
+      base: ''
+   });
+});
 
