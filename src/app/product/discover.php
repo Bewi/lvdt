b7c145f4a8085cff3007f9028dfbf524aa3e8b53
bwilfart <benjamin.wilfart@staff.voo.be>
2017-03-14 22:49:41 +0100

    Discover other products

---
+++ src/app/product/discover.php
@@ -0,0 +1,42 @@
+<div id="discover">
+    <div class="container">
+        <h1>Découvrez d'autres produits</h1>
+        <div class="row">
+            <div class="item col-xs-12 col-sm-2 col-md-2 col-lg-2">
+                <img class="thumb" src="./images/items/the-2.png" alt="" />
+                <div class="item-details">
+                    <h3>Camomille</h3>
+                    <a class="details" href="#">Détails <img src="images/detail.svg" /></a>
+                </div>
+            </div>
+            <div class="item col-xs-12 col-sm-2 col-md-2 col-lg-2">
+                <img class="thumb" src="./images/items/the-2.png" alt="" />
+                <div class="item-details">
+                    <h3>Camomille</h3>
+                    <a class="details" href="#">Détails <img src="images/detail.svg" /></a>
+                </div>
+            </div>
+            <div class="item col-xs-12 col-sm-2 col-md-2 col-lg-2">
+                <img class="thumb" src="./images/items/the-2.png" alt="" />
+                <div class="item-details">
+                    <h3>Camomille</h3>
+                    <a class="details" href="#">Détails <img src="images/detail.svg" /></a>
+                </div>
+            </div>
+            <div class="item col-xs-12 col-sm-2 col-md-2 col-lg-2">
+                <img class="thumb" src="./images/items/the-2.png" alt="" />
+                <div class="item-details">
+                    <h3>Camomille</h3>
+                    <a class="details" href="#">Détails <img src="images/detail.svg" /></a>
+                </div>
+            </div>
+            <div class="item col-xs-12 col-sm-2 col-md-2 col-lg-2">
+                <img class="thumb" src="./images/items/the-2.png" alt="" />
+                <div class="item-details">
+                    <h3>Camomille</h3>
+                    <a class="details" href="#">Détails <img src="images/detail.svg" /></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
