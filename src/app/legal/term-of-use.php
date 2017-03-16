e0a6d10655d614a916d633b55f391174c10b2580
bwilfart <benjamin.wilfart@staff.voo.be>
2017-03-16 23:28:59 +0100

    Legal pages

---
+++ src/app/legal/term-of-use.php
@@ -0,0 +1,11 @@
+<div id="legal">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-5">
+                <h3>Conditions d'utilisation</h3>
+                <p>Les données entrées par le client ne servent qu'au traitement des commandes. En vertu de la loi n° 78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, le client dispose auprès de "Le vent des thés" d'un droit d'accès, de consultation, de modification, de rectification et de suppression des données que le client nous a communiqué. </p>
+                <h3>Numéro de déclaration à la CNIL : 128 255 5</h3>
+            </div>
+        </div>
+    </div>
+</div>
--- src/index.php
+++ src/index.php
@@ -1,35 +1,12 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Le Vent Des Thés</title>
-    <meta name="description" content="Magasin de the - cafe sur Dunkerque(59). Vente en ligne de plus de 300 sortes de th�s et infusions et de divers produits et accessoires dans le domaine du the et du cafe.">
-    <meta name="robots" content="all">
-    <meta name="keywords" content="Le vent des thés, vent des thés, vdt, thé, café, salon de thé,vente , vent">
-    <meta http-equiv="Content-Language" content="fr">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="./css/bootstrap.min.css" />
-    <link rel="stylesheet" href="./css/app.css" />
-</head>
-
-<body>
-    <?php 
-        $isLightHeader = false;
-        $isAccountPage = false;
-    ?>
-
-    <?php include('./app/header/header.php') ?> 
-
-    <?php include('./app/home/nav.php') ?>   
-
-    <?php include('app/home/big-picture.php'); ?>
-
-    <?php include('app/footer/footer.php'); ?>
-
-    <script type="text/javascript" src="./scripts/app.js"></script>
-</body>
-
+<html>
+    <header></header>
+    <body>
+        <a href="/index-home.php">Home</a><br />
+        <a href="/index-perso.php">Espace perso</a><br />
+        <a href="/index-search.php">Resultat recherche</a><br />
+        <a href="/index-product.php">Détail produit</a><br />
+        <a href="/index-legal.php">Légal</a><br />
+        <a href="/index-term-of-use.php">Conditions d'utilisation</a><br />
+        <a href="/index-term-of-sales.php">Conditions de vente</a><br />
+    </body>
 </html>
