99e3885c88605c1e8d8f702406fc7aa01949daeb
bewi <benjamin.wilfart@gmail.com>
2017-03-26 10:17:47 +0200

    favico

--- src/index-term-of-use.php
+++ src/index-term-of-use.php
@@ -10,7 +10,7 @@
     <meta http-equiv="Content-Language" content="fr">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="favicon.ico" />
+    <link rel="icon" href="favico.ico" />
 
     <link rel="stylesheet" href="./css/bootstrap.min.css" />
     <link rel="stylesheet" href="./css/app.css" />
