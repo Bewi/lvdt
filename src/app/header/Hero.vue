d9d2f6e2d7f6731004054884725a59a9b75347ca
bewi <benjamin.wilfart@gmail.com>
2017-04-07 18:18:58 +0200

    logoLinkFix

--- src/app/header/Hero.vue
+++ src/app/header/Hero.vue
@@ -3,7 +3,7 @@
             <div class="container">
             <div class="row">
                 <div class="col-xs-5 col-sm-3 col-md-2 col-lg-2">
-                    <a href="/"><img src="./images/logo.svg" /></a>
+                    <router-link to='/'><img src="./images/logo.svg" /></router-link>
                 </div>
                 <div id="account" v-if="!hideAccountLink" class="col-xs-offset-2 col-sm-offset-5 col-md-offset-7 col-xs-3 col-sm-2 col-md-1 col-lg-1"
                     @click="showLogin"> 
