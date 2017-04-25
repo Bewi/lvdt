c27cf32e8136b26062b3aa89008f992cbe9a71cd
bwilfart <benjamin.wilfart@staff.voo.be>
2017-04-25 22:29:50 +0200

    loader

--- src/app/layout/Loader.vue
+++ src/app/layout/Loader.vue
@@ -1,20 +1,8 @@
 <template>
     
-<div class="loader">
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-    <defs>
-        <filter id="goo">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 35 -10" result="goo" />
-        <feBlend in="SourceGraphic" in2="goo" operator="atop" />
-        </filter>
-    </defs>
-    </svg>
+<div class="spinner">
+  <div class="double-bounce1"></div>
+  <div class="double-bounce2"></div>
 </div>
 
 </template>
