c170f2958750542f9cb260bd4b25702a5522292c
Unknown <benjamin.wilfart@gmail.com>
2017-07-05 20:33:55 +0200

    Init password layout

---
+++ src/initPassword.php
@@ -0,0 +1,43 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>Le Vent Des Thés</title>
+        <meta name="description" content="Magasin de the - cafe sur Dunkerque(59). Vente en ligne de plus de 300 sortes de th�s et infusions et de divers produits et accessoires dans le domaine du the et du cafe.">
+        <meta name="robots" content="all">
+        <meta name="keywords" content="Le vent des thés, vent des thés, vdt, thé, café, salon de thé,vente , vent">
+        <meta http-equiv="Content-Language" content="fr">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="./favico.png" />
+        <link rel="stylesheet" href="./css/bootstrap.min.css" />
+        <link rel="stylesheet" href="./css/app.css" />
+    </head>
+
+    <body class="no-footer">
+        <div id="init-password" class="black-card">
+            <div class="container">
+                <header>
+                    <img id="logo" src="./images/logo-light.svg" alt="Le vent des thés"></img>
+                    <h1>Finaliser la création de votre compte en choisisant votre mot de passe :</h1>
+                </header>
+                <div class="row">
+                    <form>
+                        <div class="input-group">
+                            <label>Rentrer votre mot de passe</label>
+                            <input id="password" type="password" />
+                        </div>
+                        <div class="input-group">
+                            <label>Verifier votre mot de passe</label>
+                            <input id="passwordConfirmation" type="password" />
+                        </div>
+                        <div class="input-group">
+                            <input type="submit" value="valider"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
